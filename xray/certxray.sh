e784386afc92c59a1171802d39f64d2ed3094458
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-01-10 07:00:46 +0800

    Update certxray.sh

--- xray/certxray.sh
+++ xray/certxray.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- https://icanhazip.com);
 clear
 echo start
 sleep 0.5
-source /var/lib/tarapkuhing/ipvps.conf
+source /var/lib/trapkuhing/ipvps.conf
 domain=$(cat /etc/xray/domain)
-sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
-cd /root/
-wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+echo -e "[ ${green}INFO${NC} ] Start " 
+sleep 1
+Cek=$(lsof -i:80 | cut -d' ' -f1 | awk 'NR==2 {print $1}')
+if [[ ! -z "$Cek" ]]; then
+sleep 1
+echo -e "[ ${red}WARNING${NC} ] Detected port 80 used by $Cek " 
+systemctl stop $Cek
+sleep 2
+echo -e "[ ${green}INFO${NC} ] Processing to stop $Cek " 
+sleep 1
+fi
+echo -e "[ ${green}INFO${NC} ] Starting renew cert... " 
+sleep 2
+echo -e "[ ${green}INFO$NC ] Getting acme for cert"
+curls acme.sh/acme.sh
 bash acme.sh --install
+/root/.acme.sh/acme.sh --upgrade --auto-upgrade >/dev/null 2>&1
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt >/dev/null 2>&1
+/root/.acme.sh/acme.sh --issue -d $domain --standalone --force --keylength ec-256 --server letsencrypt
+#/root/.acme.sh/acme.sh --issue -d $domain --standalone --force --keylength ec-256 --server letsencrypt --listen-v6
+/root/.acme.sh/acme.sh --installcert -d $domain --ecc --fullchainpath /etc/ssl/private/fullchain.pem --keypath /etc/ssl/private/privkey.pem
+echo -e "[ ${green}INFO${NC} ] Renew cert done... "
+sleep 5
 rm acme.sh
-cd .acme.sh
-echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
-bash acme.sh --register-account -m merahjambo@gmail.com
-bash acme.sh --issue --standalone -d $domain --force
-bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+echo -e "[ ${green}INFO${NC} ] Renew cert done... "
+restart

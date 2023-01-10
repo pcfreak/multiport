f1136ce4e4bf4def4eddc9faa75717b1d2163b56
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-01-10 16:52:47 +0800

    Update certxray.sh

--- xray/certxray.sh
+++ xray/certxray.sh
@@ -1,28 +1,28 @@
-#! /bin/bash
-# ======================================
-# Warna
-MERAH= ' \033[0;31m '
-NC= ' \033[0m '
-HIJAU= ' \033[0;32m '
-JERUK= ' \033[0;33m '
-BIRU= ' \033[0;34m '
-UNGU= ' \033[0;35m '
-CYAN= ' \033[0;36m '
-CAHAYA= ' \033[0;37m '
-# ======================================
-# Mendapatkan
-jernih
-mulai gema
-tidur 0,5
-sumber /var/lib/tarapkuhing/ipvps.conf
-domain= $( kucing /etc/xray/domain )
-sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs membunuh
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+clear
+echo start
+sleep 0.5
+source /var/lib/tarapkuhing/ipvps.conf
+domain=$(cat /etc/xray/domain)
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 cd /root/
 wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
-bash acme.sh --instal
+bash acme.sh --install
 rm acme.sh
 cd .acme.sh
-echo  " mulai...., Port 80 Akan di Hentikan Saat Proses install Cert "
+echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
 bash acme.sh --register-account -m merahjambo@gmail.com
 bash acme.sh --issue --standalone -d $domain --force
 bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
